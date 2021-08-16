1ec2b6466f6b7b2804e1b8e1c4dfda0eb51b0892
Nitesh Negi <nitesh.negi@quokkalabs.com>
2021-08-16 19:30:31 +0530

    components for payment success and cancel

---
+++ src/modules/PaymentCancel/index.tsx
@@ -0,0 +1,9 @@
+const PaymentCancel = () => {
+    return (
+        <div>
+            <h2 className="text-center">Payment Cancelled</h2>
+        </div>
+    )
+}
+
+export default PaymentCancel;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import React, { LazyExoticComponent } from "react";
 
-const Home = React.lazy(() => import('../modules/Home'))
-const Episodes = React.lazy(() => import('../modules/Episodes'))
-const ExchangeProgram = React.lazy(() => import('../modules/ExchangeProgram'))
-const Transcripts = React.lazy(() => import('../modules/Transcripts'))
-const AboutUs = React.lazy(() => import('../modules/AboutUs'))
+const Home = React.lazy(() => import('../modules/Home'));
+const Episodes = React.lazy(() => import('../modules/Episodes'));
+const ExchangeProgram = React.lazy(() => import('../modules/ExchangeProgram'));
+const Transcripts = React.lazy(() => import('../modules/Transcripts'));
+const AboutUs = React.lazy(() => import('../modules/AboutUs'));
+const PaymentSuccess = React.lazy(() => import('../modules/PaymentSuccess'));
+const PaymentCancel = React.lazy(() => import('../modules/PaymentCancel'));
 interface Route {
     path: string;
     exact: boolean;
@@ -48,10 +50,34 @@ const aboutUs: Route = {
     component: AboutUs
 }
 
+const paymentSuccess: Route = {
+    path: '/payments/success',
+    exact: true,
+    isProtected: false,
+    component: PaymentSuccess
+}
+
+const paymentCancel: Route = {
+    path: '/payments/cancel',
+    exact: true,
+    isProtected: false,
+    component: PaymentCancel
+}
+
+const defaultRoute: Route = {
+    path: '**',
+    exact: true,
+    isProtected: false,
+    component: Home
+}
+
 export const routeList = [
     home,
     episodes,
     exchangeProgram,
     transcripts,
-    aboutUs
+    aboutUs,
+    paymentSuccess,
+    paymentCancel,
+    defaultRoute
 ]
