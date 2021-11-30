1061b0b3ee52ccea5875c2b6b55d91f67495cce5
Nitesh Negi <nitesh.negi@quokkalabs.com>
2021-11-30 13:17:02 +0530

    404 page and coming soon page added

---
+++ src/modules/NotFound/index.tsx
@@ -0,0 +1,12 @@
+const NotFound = () => {
+    return (
+        <div className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+            <div>
+                <h1 className="text-center mb-0" style={{ fontSize: '4.5rem' }}>404</h1>
+                <h3>Page not found</h3>
+            </div>
+        </div>
+    )
+}
+
+export default NotFound;
