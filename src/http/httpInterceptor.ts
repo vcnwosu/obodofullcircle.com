7dbe19b07fa5f2b2338f41ee059c66dee89a95ff
prakash.thakur <prakash.thakur@quokkalabs.com>
2023-06-20 14:59:19 +0530

    (feat)[IGBO-1115]: Update version no of base url

--- src/http/httpInterceptor.ts
+++ src/http/httpInterceptor.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from "axios";
 import { toast } from "react-toastify";
 
-export const baseURL = 'https://apis.obodofullcircle.com/api/v1/'; // prod url
+export const baseURL = 'https://apis.obodofullcircle.com/api/v2/'; // prod url
 export const websiteURL = 'https://obodofullcircle.com/' // prod website url
 
 const axiosInstance = axios.create({
