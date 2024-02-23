0954c8d232630edfacad6bbe873f173bfd0079dc
PrakashThakur47 <prakash.thakur@quokkalabs.com>
2024-02-23 17:00:04 +0530

    (fix): Transcripts list & stream ep CTA

--- src/store/EpisodeContext.tsx
+++ src/store/EpisodeContext.tsx
@@ -1,48 +1,52 @@
 import { createContext, useState, useEffect, Context } from "react";
 import { toast } from "react-toastify";
-import { getRequest } from '../http/httpService';
+import { getRequest } from "../http/httpService";
 import { AudioCardType } from "../modules/Episodes/components/AllEpisodes/components/AudioCard";
 import CustomSpinner from "../shared/components/Spinner";
 
 export interface Season {
-    episodes: AudioCardType[];
-    season_id: number;
+  episodes: AudioCardType[];
+  season_id: number;
 }
 const EpisodeContext: Context<any> = createContext({
-    seasonList: []
+  seasonList: [],
 });
 
 export const EpisodeContextProvider = (props: any) => {
-    const [seasonList, setSeasonList] = useState<Season[]>([]);
-    const [loading, setLoading] = useState(false);
-    useEffect(() => {
-        getEpisodeList();
-    }, [])
+  const [seasonList, setSeasonList] = useState<Season[]>([]);
+  const [loading, setLoading] = useState(false);
 
-    const getEpisodeList = () => {
-        setLoading(true);
-        getRequest('get-episodes')
-            .then(res => {
-                setLoading(false);
-                if (res.data.code >= 1000 && res.data.code <= 2000) {
-                    toast.error(res.data.message);
-                } else {
-                setSeasonList(() => res.data.data);
-                }
-            })
+  const getEpisodeList = () => {
+    setLoading(true);
+    getRequest("get-episodes").then((res) => {
+      setLoading(false);
+      if (res.data.code >= 1000 && res.data.code <= 2000) {
+        toast.error(res.data.message);
+      } else {
+        // const previousSecondElementOfTheArray = res?.data?.data.filter(
+        //   (item: any) => item.season_id
+        // );
+        setSeasonList(() => res.data?.data);
+      }
+    });
+  };
 
-    }
-    const context = {
-        seasonList
-    };
-    return (
-        <>
-            <EpisodeContext.Provider value={context}>
-                {props.children}
-            </EpisodeContext.Provider>
-            <CustomSpinner show={loading} />
-        </>
-    )
-}
+  useEffect(() => {
+    getEpisodeList();
+  }, []);
+
+  const context = {
+    seasonList,
+  };
+
+  return (
+    <>
+      <EpisodeContext.Provider value={context}>
+        {props.children}
+      </EpisodeContext.Provider>
+      <CustomSpinner show={loading} />
+    </>
+  );
+};
 
 export default EpisodeContext;
