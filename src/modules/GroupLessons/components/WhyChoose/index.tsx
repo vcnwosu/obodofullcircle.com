ede94e363c28ab13b1c1ac595f170a586c030627
QL-Rohit-Ranjan <138222559+QL-Rohit-Ranjan@users.noreply.github.com>
2024-08-05 10:47:56 +0530

    (feat): text updated

--- src/modules/GroupLessons/components/WhyChoose/index.tsx
+++ src/modules/GroupLessons/components/WhyChoose/index.tsx
@@ -11,22 +11,22 @@ export interface AccordionType {
 
 const accordionArray: AccordionType[] = [
     {
-        title: 'Learn with Native Speakers',
-        text: 'Effectively decode the complexities of the language with our team of trained native speakers and bonafide tutors. Receive the support needed to gain the confidence to speak and utilize the language in your everyday life.',
+        title: 'Personalized Group Environment',
+        text: 'Unlock the advantages of collaborative learning, while still enjoying the opportunity for personalized feedback.',
         eventKey: "0"
     },
     {
-        title: 'Set your Learning Pace',
+        title: 'Group Lessons For All',
         text: 'Learn the Igbo language now at your own pace without compromising on the results and with encouraging accountability along the way. Get access to all available multimedia resources and use our innovative weekly plans as your roadmap to keep you on track.',
         eventKey: "1"
     },
     {
-        title: 'Hands-on Practical Learning',
+        title: 'Empowering Learning Environment',
         text: 'Learning any language without practicing how it is actually spoken can keep you from making progress. Learn to navigate real-life scenarios and practice the art of “thinking” more in the Igbo language through our fun and interactive exercises, created to help you advance at every stage of learning as well as deepen your understanding of cultural nuances.',
         eventKey: "2"
     },
     {
-        title: 'Affordable and Customizable Classes',
+        title: 'Innovative Approach and Curriculum',
         text: 'Have you ever taken a language class for a while and, soon after its completion, you forget everything you’ve learned and also lose your consistency with interacting with the language? With affordable prices,  diverse resources, and customizable timed sessions with your Igbo instructor, making real progress with your language learning has never been easier!',
         eventKey: "3"
     }
